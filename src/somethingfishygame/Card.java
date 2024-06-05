88a068342b01e9cce14ade4b7e666e321e433ac5
malikmem <malikmem@sheridancollege.ca>
2024-06-04 21:40:24 -0400

    Initial outline of the code for SomethingFishy.
    Memoona

---
+++ src/somethingfishygame/Card.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package somethingfishygame;
+
+/**
+ *
+ * @author memoonamalik
+ */
+public class Card {
+    private final String suit;
+    private final String rank;
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
---
+++ src/somethingfishygame/SomethingFishyGame.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package somethingfishygame;
+
+/**
+ *
+ * @author memoonamalik
+ */
+import java.util.Scanner;
+
+public class SomethingFishyGame {
+    private final Deck deck;
+    private final Player player;
+    private final Player computer;
+
+    public SomethingFishyGame() {
+        deck = new Deck();
+        player = new Player("Player");
+        computer = new Player("Computer");
+        dealInitialCards();
+    }
+
+    private void dealInitialCards() {
+        for (int i = 0; i < 7; i++) {
+            player.addCardToHand(deck.drawCard());
+            computer.addCardToHand(deck.drawCard());
+        }
+    }
+
+    private void play() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (deck.getSize() > 0) {
+            // Player's turn
+            System.out.println("Your hand: " + player.getHand());
+            System.out.print("Ask for a rank: ");
+            String rank = scanner.nextLine();
+
+            Card card = computer.removeCardFromHand(rank);
+            if (card != null) {
+                System.out.println("Computer has a " + card);
+                player.addCardToHand(card);
+            } else {
+                System.out.println("Go Fish!");
+                player.addCardToHand(deck.drawCard());
+            }
+
+           
+        }
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        SomethingFishyGame game = new SomethingFishyGame();
+        game.play();
+    }
+}
